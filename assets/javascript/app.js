43026b695e22b74ab4ee9eb14a1d8c3e053975d3
Victor Tsang <victorwltsang@gmail.com>
2017-03-05 18:18:26 -0800

    change to https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -53,7 +53,7 @@ $(document).ready(function() {
 
             var api_key = "dc6zaTOxFJmzC";
             var limit = 10;
-            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + myAnimal + "&limit=" + limit + "&api_key=" + api_key;
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + myAnimal + "&limit=" + limit + "&api_key=" + api_key;
 
             $.ajax({
                 url: queryURL,
